f45f54c0c98af395ec9fd5d2cb15896b3ace0613
Pâyidar <79402324+payidarofficial@users.noreply.github.com>
2021-03-07 16:56:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+isim = str(input('Lütfen İsminizi Giriniz '))
+yas = int(input('Lütfen Yaşınızı Giriniz '))
+
+# Pâyidar ---
+if(isim, yas):
+    print(f'Duyduğuma göre ismin `{isim}` yaşın ise `{yas}`')
